6d5efcf706ab0e28ed94a42bf874f568dd4dfbdc
Renata Guttenová <guttenova.renata@gmail.com>
2017-05-20 15:20:11 +0200

    Initial Commit

---
+++ Feather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Feather
+//
+//  Created by Renata Guttenová on 20/05/2017.
+//  Copyright © 2017 Renata Guttenová. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Feather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Feather
+//
+//  Created by Renata Guttenová on 20/05/2017.
+//  Copyright © 2017 Renata Guttenová. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
